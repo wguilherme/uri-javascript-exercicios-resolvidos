1869650daf6a3477fbd36ca0060e9070b3daa08c
W. Guilherme <ravok123@gmail.com>
2019-12-01 23:07:29 -0300

    1011

---
+++ INICIANTE/1011.js
@@ -0,0 +1,14 @@
+/* Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R).
+A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++),
+assumem que o resultado da divisão entre dois inteiros é outro inteiro.*/
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var raio = parseFloat(lines.shift()).toFixed(2);
+var pi = 3.14159;
+
+var volume = (4/3) * pi * (raio**3);
+
+console.log("VOLUME = " + parseFloat(volume).toFixed(3));
