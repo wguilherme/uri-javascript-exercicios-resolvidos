f6fc19248355447107cfec408f354b7da29448c8
W. Guilherme <ravok123@gmail.com>
2019-12-01 22:25:07 -0300

    1008

---
+++ INICIANTE/1008.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var number = parseInt(lines.shift());
+var ht = parseInt(lines.shift());
+var vh = parseFloat(lines.shift()).toFixed(2);
+var salary = parseFloat(ht * vh).toFixed(2);
+
+
+console.log("NUMBER = " + number)
+console.log("SALARY = U$ " + salary);
